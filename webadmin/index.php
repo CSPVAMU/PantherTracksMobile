5a404c22642037e3a309644a022f46390c13757f
James Pryor <james@geekwagon.net>
2013-07-14 12:55:33 -0500

    Web Admin: Seperated header and footer.

--- webadmin/index.php
+++ webadmin/index.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<head>
-    <title>Degree Plan Viewer</title>
-    <link rel="stylesheet" href="css/default.css">
-    <script src="scripts/jquery-1.10.2.min.js"></script>
-    <script src="scripts/jquery.tablesorter.js"></script>    
-</head>
+<?php include("header.html"); ?>
 
-<body>
-    <div class="wrapper">
-        <h1>PT Mobile Web Admin</h1>
         <h2>Degree Plans</h2>
         <div id="plan-list">
             <a id="create-new" href="">Create New Degree Plan</a>
@@ -24,10 +15,5 @@
                 </thead>
             </table>
         </div>
-    </div><!--.wrapper-->
 
-    <script src="scripts/functions.js"></script>
-    <script src="scripts/handlers.js"></script>
-    <script src="scripts/ready.js"></script>
-</body>
-</html>
+<?php include("footer.html"); ?>
